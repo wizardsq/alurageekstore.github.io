1c5164e42bc073ac3e9e28bb430438b3cbc3bf6c
w1zardsq <gdzul33@proton.me>
2023-10-02 11:58:14 -0600

    Cambio a json local

--- app.mjs
+++ app.mjs
@@ -1,6 +1,17 @@
 const productContainer = document.getElementById("product-container");
-import { GetAllProducts } from './routes/productsRoute.mjs';
-
+async function loadJSON() {
+    try {
+        const response = await fetch('./db/products.json'); // Ajusta la ruta según tu estructura de carpetas
+        if (!response.ok) {
+            throw new Error('No se pudo cargar el archivo JSON');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error al cargar el archivo JSON:', error);
+        throw error; // Puedes manejar el error según tus necesidades
+    }
+}
 // Obtener la tarjeta de producto que se clonará
 const productCard = productContainer.querySelector(".card");
 
@@ -18,17 +29,15 @@ function crearTarjetaProducto(producto) {
     return nuevaTarjeta;
 }
 
-// Agregar cada tarjeta de producto al contenedor
-GetAllProducts().then(productos => {
-    console.log(productos);
-    // Eliminar la tarjeta clonada inicial
-    productCard.remove();
-
-    // Agregar cada tarjeta de producto al contenedor
-    productos.forEach(producto => {
-        const tarjetaProducto = crearTarjetaProducto(producto);
-        productContainer.appendChild(tarjetaProducto);
+loadJSON()
+    .then(data => {
+        const productos = data.productos; 
+        productCard.remove();// Accede a la matriz de productos dentro del objeto
+        productos.forEach(producto => {
+            const tarjetaProducto = crearTarjetaProducto(producto);
+            productContainer.appendChild(tarjetaProducto);
+        });
+    })
+    .catch(error => {
+        console.error('Ocurrió un error al cargar el archivo JSON:', error);
     });
-}).catch(error => {
-    console.error(error);
-});
